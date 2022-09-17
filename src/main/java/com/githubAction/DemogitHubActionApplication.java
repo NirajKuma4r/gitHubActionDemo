0f38834abc4ec9d911fc0b58979914dd7e6ba484
Testuser1 <niraj.kumar@mnscorp.net>
2022-09-17 22:03:06 +0000

    code-spring

---
+++ src/main/java/com/githubAction/DemogitHubActionApplication.java
@@ -0,0 +1,13 @@
+package com.githubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitHubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogitHubActionApplication.class, args);
+	}
+
+}
