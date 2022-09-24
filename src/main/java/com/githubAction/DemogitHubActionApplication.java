4fcefd3187a6235304fad6ce3d9ffae6583bc4db
Testuser1 <niraj.kumar@mnscorp.net>
2022-09-24 10:33:52 +0000

    updated the file

--- src/main/java/com/githubAction/DemogitHubActionApplication.java
+++ src/main/java/com/githubAction/DemogitHubActionApplication.java
@@ -2,10 +2,17 @@ package com.githubAction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemogitHubActionApplication {
 
+	@GetMapping("/showmessage")
+	public String showWelcomeMessage() {
+		return "Welcome in GitHubAction";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemogitHubActionApplication.class, args);
 	}
